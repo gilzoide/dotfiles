d57ff8fbb31b93199c2b72f18c7997aeedc46f6d
gilzoide <gilzoide@gmail.com>
2022-10-04 14:21:36 -0300

    Add generate-adaptive-icon.sh script

---
+++ shell-scripts/generate-adaptive-icon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+usage="Usage: $0 INPUT_FILE OUTPUT_FILE"
+
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+  echo "$usage"
+  exit 1
+fi
+
+ffmpeg -i "$1" -vf 'pad=iw*1.5:ih*1.5:iw/4:ih/4:color=#00000000' "$2"
