a310fabc50abf78185bb079fa180951bb39cba03
gilzoide <gilzoide@gmail.com>
2022-10-15 10:14:49 -0300

    Add macos-color-picker.sh script

---
+++ shell-scripts/macos-color-picker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Launch a macOS color picker and copy the result to clipboard
+# Result is in the format RRGGBB using hexadecimal digits (HTML style)
+
+rgb=$(osascript -e 'choose color')
+
+red_raw=$(echo "$rgb" | cut -d ',' -f 1)
+green_raw=$(echo "$rgb" | cut -d ',' -f 2)
+blue_raw=$(echo "$rgb" | cut -d ',' -f 3)
+
+red=$(echo "$red_raw / 256" | bc)
+green=$(echo "$green_raw / 256" | bc)
+blue=$(echo "$blue_raw / 256" | bc)
+
+printf '%02x%02x%02x' "$red" "$green" "$blue" | pbcopy
