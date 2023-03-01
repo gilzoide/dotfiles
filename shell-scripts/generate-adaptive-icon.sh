f21b8e8f1c43492e32bb88d9bc0e6c427a9a55d9
gilzoide <gilzoide@gmail.com>
2023-03-01 15:50:00 -0300

    Fix generate-adaptive-icon.sh specifying RGBA

--- shell-scripts/generate-adaptive-icon.sh
+++ shell-scripts/generate-adaptive-icon.sh
@@ -7,4 +7,4 @@ if [[ -z "$1" ]] || [[ -z "$2" ]]; then
   exit 1
 fi
 
-ffmpeg -i "$1" -vf 'pad=iw*1.5:ih*1.5:iw/4:ih/4:color=#00000000' "$2"
+ffmpeg -i "$1" -vf 'format=rgba,pad=iw*1.5:ih*1.5:iw/4:ih/4:color=#00000000' "$2"
